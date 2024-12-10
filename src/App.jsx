32f13494efe6d6fbbf9ec9f7da64dc7fa8b40015
faizan-cp1 <faizan.chatpandas@gmail.com>
2024-12-10 14:22:52 +0500

    Website 3 Sections in React Js + Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Benefits from './components/Benefits'
+import Hero from './components/Hero'
+import Partners from './components/partners'
+
+function App() {
+  
+
+  return (
+    <div>
+     <Hero/>
+     <Partners/>
+     <Benefits/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import info1 from '../assets/feature-1.png'
+import info2 from '../assets/feature-2.png'
+const Benefits = () => {
+  return (
+    <div id='benefits' >
+
+        {/* Info Box 1 */}
+        <div className='w-full grid grid-cols-1 lg:grid-cols-2 lg:px-24 px-6 lg:py-20 py-10 gap-8' >
+          <div className=' space-y-5' >
+            <h2 className='font-extrabold text-3xl text-primary' >Work with tools you already use
+            </h2>
+            <p className='text-secondary text-lg' >Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
+            <ul className='space-y-4 text-md' >
+                <li className='flex items-center gap-x-3  ' > <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                 <span className='font-semibold' >Continuous integration and deployment</span>
+                </li>
+                <li className='flex items-center gap-x-3  ' > <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                 <span className='font-semibold' >Development workflow</span>
+                </li>
+                <li className='flex items-center gap-x-3  ' > <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                 <span className='font-semibold' >Knowledge management</span>
+                </li>
+
+            </ul>
+            <p className='text-secondary text-lg' >Deliver great service experiences fast - 
+                without the complexity of traditional ITSM solutions. </p>
+         </div>
+
+          <div>
+            <img src={info1}  className='w-[100%] lg:w-[80%] md:w-[90%]  ' alt="" />
+          </div>
+
+        </div>
+
+        {/* Info Box 2 */}
+
+        <div className='w-full grid grid-cols-1 lg:grid-cols-2 lg:px-24 px-6 lg:py-20 py-6 gap-8' >
+          <div className=' space-y-5 order-2' >
+            <h2 className='font-extrabold text-3xl text-primary' >Work with tools you already use
+            </h2>
+            <p className='text-secondary text-lg' >Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
+            <ul className='space-y-4 text-md' >
+                <li className='flex items-center gap-x-3  ' > <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                 <span className='font-semibold' >Continuous integration and deployment</span>
+                </li>
+                <li className='flex items-center gap-x-3  ' > <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                 <span className='font-semibold' >Development workflow</span>
+                </li>
+                <li className='flex items-center gap-x-3  ' > <svg class="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                 <span className='font-semibold' >Knowledge management</span>
+                </li>
+
+            </ul>
+            <p className='text-secondary text-lg' >Deliver great service experiences fast - 
+            without the complexity of traditional ITSM solutions. </p>
+
+         </div>
+
+          <div className='lg:order-1 order-2' >
+            <img src={info2}  className='w-[100%] lg:w-[80%] md:w-[90%]  ' alt="" />
+          </div>
+
+        </div>
+
+
+
+    </div>
+    
+  )
+}
+
+export default Benefits
